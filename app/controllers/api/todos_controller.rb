a5ca1460c9a0af23ab739cb0c0fd1889343b5e1d
ashap94 <aaronshapiro94@gmail.com>
2020-01-28 20:28:23 -0800

    Added rails application to project
    added todo model and migration
    added todo controller
    in process of adding all action logic to controller
    next step will be to create routes to access said actions

---
+++ app/controllers/api/todos_controller.rb
@@ -0,0 +1,46 @@
+class Api::TodosController < ApplicationController
+
+    def index
+        # let all_todos = Todo.all
+        # going to have tp iterate through all todos, and insert into a 
+        # json servable object, do this in the jbuilder index view
+    end
+
+    def show
+        render json: Todo.find(params[:id])
+    end
+
+    def create
+        @todo = Todo.new(todo_params)
+        if @todo.save
+            render json: @todo
+        else
+            render json: @todo.errors.full_messages, status: 422
+        end
+    end
+
+    def update
+        @todo = Todo.find(params[:id])
+        if @todo.update(todo_update_params)
+            render json: @todo
+        else
+            render json: @todo.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+    end
+
+    private
+
+    def todo_params
+        params.require(:todo).permit(:title, :body, :done)
+    end
+
+    def todo_update_params
+        # using this method may cause issues in that it will only record this property
+        # and destroy the other properties? not sure but probably best to go with above method
+        params.require(:todo).permit(:done)
+    end
+
+end
